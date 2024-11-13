65cdbaa57f1fb6bf490f3c701480c9c122d2b91b
hspayde <spaydeht@hendrix.edu>
2024-11-13 13:08:08 -0600

    Created and finished InfoViewObject

---
+++ hendrix_tours_app/lib/objects/info_view_item.dart
@@ -0,0 +1,16 @@
+
+class InfoViewItem {
+  InfoViewItem({required this.title,
+  required this.description,
+  required this.imagePath,
+  required this.connBuildings,
+  required this.connDepartments,
+  required this.link,
+  });
+  String title;
+  String description;
+  String imagePath;
+  List<InfoViewItem> connBuildings;
+  List<InfoViewItem> connDepartments;
+  String link;
+}
